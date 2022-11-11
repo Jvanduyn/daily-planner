e5a2225ed2b34488da45e9bf6c84ef15c02f80e1
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-11-10 22:01:55 -0500

    worked on it with alex a little bit

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeDisplayEl = $('#currentDay');
+var past = $('.past');
+var present = $('.present');
+var future = $('.future');
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -18,6 +23,19 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  setInterval(displayTime, 1000);
 });
+
+var displayTime = function () {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+
+// notes with james
+// could do a an if else statement
+
+// if currentHour > hour then hour would be future
+// need a var for savebutton
+//you could make an array with all the working hours and compare the hours in that
+//array
