78e6a7aeae4942fb3d5b256dc4703981396dfc01
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-11-10 23:25:18 -0500

    almost finished. just need to get the local storage working

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var timeDisplayEl = $('#currentDay');
 var past = $('.past');
 var present = $('.present');
 var future = $('.future');
+var tableDiv = document.querySelectorAll("div");
+var saveButton = document.querySelectorAll("button");
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -13,13 +12,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  setTimeState();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -31,11 +24,35 @@ var displayTime = function () {
   timeDisplayEl.text(rightNow);
 }
 
+var setTimeState = function () {
+  for (let i = 0; i < tableDiv.length; i++) {
+    // console.log(dayjs().hour())
+    // console.log(tableDiv[i].id);
+    if (dayjs().hour() == tableDiv[i].id) {
+      tableDiv[i].classList.add('present');
+    } else if (dayjs().hour() < tableDiv[i].id) {
+      tableDiv[i].classList.add('future');
+    } else {
+      tableDiv[i].classList.add('past');
+    }
+  }
+}
 
-// notes with james
-// could do a an if else statement
+renderLastRegistered();
 
-// if currentHour > hour then hour would be future
-// need a var for savebutton
-//you could make an array with all the working hours and compare the hours in that
-//array
+function renderLastRegistered() {
+  var message = localStorage.getItem("fa-save");
+
+  message.textContent = message;
+}
+
+saveButton.forEach(box => {
+  box.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var message = document.querySelector("fa-save");
+
+    localStorage.setItem("fa-save", message);
+    renderLastRegistered();
+  });
+});
