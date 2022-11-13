3b6d03c974d112f5a33393e9c0f5be083f81a6bc
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-11-13 17:47:10 -0500

    working project with everything completed. Just need to add read me and deploy

--- Develop/script.js
+++ Develop/script.js
@@ -6,28 +6,20 @@ var tableDiv = document.querySelectorAll("div");
 var saveButton = document.querySelectorAll("button");
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   setTimeState();
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   setInterval(displayTime, 1000);
 });
 
+//displaying the time at the top of the page
 var displayTime = function () {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
 }
 
+//this is how we are able to determine what time state the block of hour
+//is compared to the current time
 var setTimeState = function () {
   for (let i = 0; i < tableDiv.length; i++) {
-    // console.log(dayjs().hour())
-    // console.log(tableDiv[i].id);
     if (dayjs().hour() == tableDiv[i].id) {
       tableDiv[i].classList.add('present');
     } else if (dayjs().hour() < tableDiv[i].id) {
@@ -38,21 +30,22 @@ var setTimeState = function () {
   }
 }
 
-renderLastRegistered();
-
-function renderLastRegistered() {
-  var message = localStorage.getItem("fa-save");
-
-  message.textContent = message;
-}
-
-saveButton.forEach(box => {
-  box.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    var message = document.querySelector("fa-save");
-
-    localStorage.setItem("fa-save", message);
-    renderLastRegistered();
-  });
+//this is how we are logging the local storage in each time block
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, text);
+  })
 });
+
+//this is how we are able to get the data out of local storage for each time block
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
